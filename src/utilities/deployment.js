d604c2e7922be9cca8fa2fa1c112b138b3270081
Ryan <104562337+railBitsAndHex@users.noreply.github.com>
2022-10-26 10:26:37 +0800

    feat: Backend Render deployment
    Deployed backend on Render
    Update axios urls to point to newly deployed backend

--- src/utilities/deployment.js
+++ src/utilities/deployment.js
@@ -1,1 +1,2 @@
-export const SERVER_URL = "https://us-central1-lucky-sphinx-365408.cloudfunctions.net/app"
+// export const SERVER_URL = "https://us-central1-lucky-sphinx-365408.cloudfunctions.net/app"
+export const SERVER_URL = "https://testrenderdeployv1.onrender.com"
